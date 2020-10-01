769602ab2d425d427cc4b13d2b74fc71ab89034e
ameerrah9 <mccullers.ghameerah@gmail.com>
2020-10-01 17:58:26 -0400

    Done.

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -5,6 +5,11 @@ class StoreAdminController < ApplicationController
     end
 
     def orders
+        render :layout => "order_administration"
+    end
+
+    def invoice
+        render :layout => false
 
     end
 end

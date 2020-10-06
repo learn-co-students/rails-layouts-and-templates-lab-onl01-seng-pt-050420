daace172c8bb747ebcd37abaf34c214de5c3936d
tpham13 <tpham0123@gmail.com>
2020-10-06 11:24:15 -0700

    Done.

---
+++ app/controllers/store_admin_controller.rb
@@ -0,0 +1,14 @@
+class StoreAdminController < ApplicationController
+    layout 'admin'
+    def home
+    end
+  
+    def orders
+      render :layouts => "order_administration"
+    end
+  
+    def invoice
+      render :layout => false
+    end
+  end
+  
